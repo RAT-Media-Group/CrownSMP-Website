52f766c82125c6412653a4809b2061f682b2743b
105hua <joshuajtudball2003@gmail.com>
2023-02-07 09:45:59 +0000

    Done for now, not sure what else to add.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Button } from '@mui/material';
 import './App.css';
+
+const copyButtonFunction = () => {
+  navigator.clipboard.writeText("play.crownnetwork.org");
+  alert("Your IP should now be in your clipboard, enjoy!");
+}
 
 function App() {
   return (
-    <div className="App">
+    <div className="centerContainer">
 
-      <h1 className='title'>CrownSMP</h1>
-      
-      <button className='connectButton' type="button">Copy IP</button>
+      <h1 className='title glow'>Crown Network</h1>
+      <h2></h2>
+      <Button className='copyButton' variant="text" onClick={copyButtonFunction}>Copy IP</Button>
+
 
     </div>
   )
