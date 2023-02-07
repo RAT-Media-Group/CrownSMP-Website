e40ed6419a3b74af66b1e1bac4f63caa23bac7fb
105hua <joshuajtudball2003@gmail.com>
2023-02-07 01:32:12 +0000

    First Commit.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1 className='title'>CrownSMP</h1>
+      
+      <button className='connectButton' type="button">Copy IP</button>
+
+    </div>
+  )
+}
+
+export default App
